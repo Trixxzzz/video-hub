2e63d5c6404af5c9b8ba985298700a1674ff59ff
Trixxzzz <ramtekearyan2003@gmail.com>
2023-10-20 20:36:22 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import {Box, Button, HStack, Heading, Stack, VStack,Input,Text} from "@chakra-ui/react"
+import {AiOutlineSend} from "react-icons/ai"
+
+const Footer = () => {
+  return (
+   <Box  bgColor={"blackAlpha.900"} minH={"40"} p={"16"} color={"white"}>  
+   <Stack direction={["column","row"]}>
+    <VStack alignItems={"stretch"} w={"full"} p={"4"}>
+        <Heading size={"md"} transform={"uppercase"} textAlign={["center","left"]}>
+            Subscribe to get updates!
+        </Heading>
+        <HStack borderBottom={"2px solid white"} py={"2"}>
+            <Input placeholder='Enter Email Here....' border={"none"} borderRadius={"none"} outline={"none"} focusBorderColor='none'/>
+            <Button p={"0"} colorScheme={"purple"} variant={"ghost"} borderRadius={"0 20px 20px 0"}>
+                <AiOutlineSend size={"20"}/>
+            </Button>
+        </HStack>
+    </VStack>
+
+    <VStack w={"full"} 
+    borderLeft={["none","1px solid white"]}
+    borderRight={["none","1px solid white"]}>
+        <Heading textTransform={"uppercase"} textAlign={"center"}>
+            VIDEO HUB
+        </Heading>
+        <Text>All rights received</Text>
+
+    </VStack>
+
+    <VStack w={"full"}>
+    <Heading size={"md"} textTransform={"uppercase"}>Social Media</Heading>
+    <Button variant={"link"} colorScheme={'white'}>
+        <a target={"blank"} href= "https://instagram.com/its_.aryannn._">Instagram</a>
+    </Button>
+    <Button variant={"link"} colorScheme={'white'}>
+        <a target={"blank"}href= "https://github.com/Trixxzzz">Github</a>
+    </Button>
+    <Button variant={"link"} colorScheme={'white'}>
+        <a target={"blank"}href= "hhttps://www.linkedin.com/in/ramteke-aryan-ratnaghosh-79334422a/">LinkedIn</a>
+    </Button>
+    </VStack>
+   </Stack>
+    </Box>
+  )
+}
+
+export default Footer
